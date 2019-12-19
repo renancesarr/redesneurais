e502fd0778eaf0c29a323ac21538043d21045805
Renan Cesar Barbosa Rabelo <renan.cesar.r@gmail.com>
2019-12-19 02:27:52 -0300

    aperfeiçoando com numpy

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,19 +1,17 @@
-
+import numpy as np
 
 def soma(e, p):
-    s = 0
-    for i in range(len(e)):
-        s = s + e[i]*p[i]
-    return s
+    return e.dot(p)
 
 def stepFunction(soma):
     if(soma>=1):
         return 1
     return 0
 
-entradas = [1 , 7 , 5]
-pesos = [0.8, 0.1, 0]
+entradas = np.array([1 , 7 , 5])
+pesos = np.array([0.8, 0.1, 0])
 
 s = soma(entradas, pesos)
 
 r = stepFunction(s)
+print(r)
